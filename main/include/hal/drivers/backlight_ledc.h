3115aad3c2099698a2bf56371995898bf08da03e
gifbadge <gifbadge@gifbadge.ca>
2024-01-07 18:39:54 -0500

    Add missing files

---
+++ main/include/hal/drivers/backlight_ledc.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "hal/backlight.h"
+
+class backlight_ledc: public Backlight{
+public:
+    backlight_ledc(gpio_num_t gpio, int level = 100);
+    ~backlight_ledc() override = default;
+    void state(bool) override;
+    void setLevel(int) override;
+    int getLevel() override;
+private:
+    int lastLevel;
+};
