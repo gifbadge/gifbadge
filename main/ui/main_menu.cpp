bfbd8850dd04b33c3c6a3c35a059e29d3f95106b
gifbadge <gifbadge@gifbadge.ca>
2024-02-15 14:55:32 -0500

    Add storage ui

--- main/ui/main_menu.cpp
+++ main/ui/main_menu.cpp
@@ -8,6 +8,7 @@
 #include "display.h"
 #include "ui/style.h"
 #include "ui/device_group.h"
+#include "ui/storage.h"
 
 
 static void exit_callback(lv_event_t *e){
@@ -33,7 +34,13 @@ static void file_select_callback(lv_event_t *e){
     lv_obj_add_event_cb(file_options_window, file_options_close, LV_EVENT_DELETE, obj);
 }
 
-
+static void storage_callback(lv_event_t *e){
+    auto * obj = static_cast<lv_obj_t *>(lv_event_get_target(e));
+    LV_LOG_USER("%s", lv_label_get_text(obj));
+    lv_screen_load(lv_obj_create(nullptr));
+    lv_obj_t *window = storage_menu();
+    lv_obj_add_event_cb(window, file_options_close, LV_EVENT_DELETE, obj);
+}
 
 
 void main_menu()
@@ -46,11 +53,18 @@ void main_menu()
         lv_obj_t *file_label = lv_label_create(file_btn);
         lv_obj_add_style(file_label, &menu_font_style, LV_PART_MAIN);
         lv_label_set_text(file_label, "File Select");
+
+        lv_obj_t *storage_btn = lv_file_list_add(main_menu, nullptr);
+        lv_obj_t *storage_label = lv_label_create(storage_btn);
+        lv_obj_add_style(storage_label, &menu_font_style, LV_PART_MAIN);
+        lv_label_set_text(storage_label, "Storage");
+
         lv_obj_t *exit_btn = lv_file_list_add(main_menu, nullptr);
         lv_obj_t *exit_label = lv_label_create(exit_btn);
         lv_obj_add_style(exit_label, &menu_font_style, LV_PART_MAIN);
         lv_label_set_text(exit_label, "Exit");
         lv_obj_add_event_cb(file_label, file_select_callback, LV_EVENT_CLICKED, nullptr);
+        lv_obj_add_event_cb(storage_label, storage_callback, LV_EVENT_CLICKED, nullptr);
         lv_obj_add_event_cb(exit_label, exit_callback, LV_EVENT_CLICKED, nullptr);
         lv_file_list_scroll_to_view(main_menu, 0);
         lvgl_unlock();
---
+++ main/ui/storage.cpp
@@ -0,0 +1,74 @@
+#include "ui/storage.h"
+#include "ui/lvgl.h"
+#include "ui/device_group.h"
+#include "ui/widgets/file_list/file_list.h"
+#include "ui/style.h"
+#include "hw_init.h"
+
+static void StorageExit(lv_event_t *e) {
+    lv_obj_del(static_cast<lv_obj_t *>(lv_event_get_user_data(e)));
+}
+
+lv_obj_t *storage_menu(){
+    if (lvgl_lock(-1)) {
+
+        auto config = ImageConfig();
+
+        new_group();
+        lv_obj_t *cont_flex = lv_file_list_create(lv_scr_act());
+        lv_file_list_icon_style(cont_flex, &icon_style);
+
+        StorageInfo info = get_board()->storageInfo();
+
+        if(info.name){
+            lv_obj_t *name_btn = lv_file_list_add(cont_flex, nullptr);
+            lv_obj_t *name_label = lv_label_create(name_btn);
+            lv_label_set_text(name_label, info.name);
+        }
+        lv_obj_t *type_btn = lv_file_list_add(cont_flex, nullptr);
+        lv_obj_t *type_label = lv_label_create(type_btn);
+        switch(info.type){
+
+            case StorageType_None:
+                lv_label_set_text(type_label, "None");
+                break;
+            case StorageType_SPI:
+                lv_label_set_text(type_label, "Internal Flash");
+                break;
+            case StorageType_SDIO:
+                lv_label_set_text(type_label, "SDIO");
+                break;
+            case StorageType_MMC:
+                lv_label_set_text(type_label, "MMC");
+                break;
+            case StorageType_SD:
+                lv_label_set_text(type_label, "SD");
+                break;
+            case StorageType_SDHC:
+                lv_label_set_text(type_label, "SDHC/SDXC");
+                break;
+        }
+        lv_obj_t *speed_btn = lv_file_list_add(cont_flex, nullptr);
+        lv_obj_t *speed_label = lv_label_create(speed_btn);
+        lv_label_set_text_fmt(speed_label, "%.0fMhz", info.speed);
+
+        lv_obj_t *size_btn = lv_file_list_add(cont_flex, nullptr);
+        lv_obj_t *size_label = lv_label_create(size_btn);
+        lv_label_set_text_fmt(size_label, "%.2f/%.2f", (double)(info.total_bytes-info.free_bytes)/1000/1000, (double)(info.total_bytes)/1000/1000);
+
+        lv_obj_t *format_btn = lv_file_list_add(cont_flex, nullptr);
+        lv_obj_t *format_label = lv_label_create(format_btn);
+        lv_label_set_text(format_label, "Format Storage");
+
+        lv_obj_t *exit_button = lv_file_list_add(cont_flex, "\ue5c9");
+        lv_obj_t *exit = lv_obj_create(exit_button);
+        lv_obj_add_flag(exit, LV_OBJ_FLAG_HIDDEN);
+        lv_group_remove_obj(exit);
+
+        lv_obj_add_event_cb(exit, StorageExit, LV_EVENT_CLICKED, cont_flex);
+
+        lvgl_unlock();
+        return cont_flex;
+    }
+    return nullptr;
+}
