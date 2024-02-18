dcefc37d2ede3399242ef4a3245754fa7beb7e07
gifbadge <gifbadge@gifbadge.ca>
2024-02-17 20:15:59 -0500

    Quiet logging

--- main/file_util.cpp
+++ main/file_util.cpp
@@ -15,14 +15,14 @@ std::vector<std::string> list_directory(const std::string &path) {
             }
             std::string current_path = (path + "/" + de->d_name);
             if (current_path.starts_with("/data")) {
-                ESP_LOGI(TAG, "%s", current_path.c_str());
+                ESP_LOGV(TAG, "%s", current_path.c_str());
                 std::string fat_path = current_path.substr(5, current_path.length());
-                ESP_LOGI(TAG, "%s %s", current_path.c_str(), fat_path.c_str());
+                ESP_LOGV(TAG, "%s %s", current_path.c_str(), fat_path.c_str());
                 //Check if file is hidden, a directory, or has any ignored characters.
                 //This will only work on the first FatFS filesystem right now, I think?
                 FILINFO info;
                 if (f_stat(fat_path.c_str(), &info) == 0) {
-                    ESP_LOGI(TAG, "%s %i %i", current_path.c_str(), info.fattrib & AM_DIR, info.fattrib & AM_HID);
+                    ESP_LOGV(TAG, "%s %i %i", current_path.c_str(), info.fattrib & AM_DIR, info.fattrib & AM_HID);
                     if (!((info.fattrib & AM_DIR) || (info.fattrib & AM_HID))) {
                         if (de->d_name[0] != '.' && de->d_name[0] != '~') {
                             files.emplace_back(current_path);
@@ -47,14 +47,14 @@ std::vector<std::string> list_folders(const std::string &path) {
             }
             std::string current_path = (path + "/" + de->d_name);
             if (current_path.starts_with("/data")) {
-                ESP_LOGI(TAG, "%s", current_path.c_str());
+                ESP_LOGV(TAG, "%s", current_path.c_str());
                 std::string fat_path = current_path.substr(5, current_path.length());
-                ESP_LOGI(TAG, "%s %s", current_path.c_str(), fat_path.c_str());
+                ESP_LOGV(TAG, "%s %s", current_path.c_str(), fat_path.c_str());
                 //Check if file is hidden or has any ignored characters.
                 //This will only work on the first FatFS filesystem right now, I think?
                 FILINFO info;
                 if (f_stat(fat_path.c_str(), &info) == 0) {
-                    ESP_LOGI(TAG, "%s %i %i", current_path.c_str(), info.fattrib & AM_DIR, info.fattrib & AM_HID);
+                    ESP_LOGV(TAG, "%s %i %i", current_path.c_str(), info.fattrib & AM_DIR, info.fattrib & AM_HID);
                     if ((info.fattrib & AM_DIR) && !(info.fattrib & AM_HID)) {
                         if (de->d_name[0] != '.' && de->d_name[0] != '~') {
                             folders.emplace_back(current_path);
