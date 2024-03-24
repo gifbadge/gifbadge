a06fc613bc1fb47a5fc0615da19ab036409b2636
gifbadge <gifbadge@gifbadge.ca>
2024-03-24 15:12:11 -0400

    efuse table

---
+++ main/include/esp_efuse_custom_table.h
@@ -0,0 +1,24 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "esp_efuse.h"
+
+// md5_digest_table 4a1734a4aed931971168b0384ae86118
+// This file was generated from the file esp_efuse_custom_table.csv. DO NOT CHANGE THIS FILE MANUALLY.
+// If you want to change some fields, you need to change esp_efuse_custom_table.csv file
+// then run `efuse_common_table` or `efuse_custom_table` command it will generate this file.
+// To show efuse_table run the command 'show_efuse_table'.
+
+
+extern const esp_efuse_desc_t* ESP_EFUSE_USER_DATA_BOARD[];
+
+#ifdef __cplusplus
+}
+#endif
