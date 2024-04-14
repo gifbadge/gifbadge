345198078d5f5e39c74f5a734ddd2f976be10571
gifbadge <gifbadge@gifbadge.ca>
2024-04-14 16:00:40 -0400

    fix board data generation

--- main/hal/boards/board_data.c
+++ main/hal/boards/board_data.c
@@ -1,9 +1,11 @@
 #include <stdint.h>
 #include "ota.h"
 #include "hal/boards/boards.h"
+#include "sdkconfig.h"
 
-#ifdef CONFIG_SPIRAM_MODE_OCT
+
+#if CONFIG_SPIRAM_MODE_OCT
 const __attribute__((section(".rodata_custom_desc"))) esp_custom_app_desc_t custom_app_desc = {1, {BOARD_2_1_V0_2}};
-#else
+#elif CONFIG_SPIRAM_MODE_QUAD
 const __attribute__((section(".rodata_custom_desc"))) esp_custom_app_desc_t custom_app_desc = {2, {BOARD_1_28_V0, BOARD_1_28_V0_1}};
 #endif
