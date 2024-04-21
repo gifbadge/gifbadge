c8df5322da9a51176d5f39fecd656c124b3afe71
gifbadge <gifbadge@gifbadge.ca>
2024-04-20 22:10:22 -0400

    Add board_2_1_v0_4

---
+++ main/include/hal/boards/board_2_1_v0_4.h
@@ -0,0 +1,56 @@
+#pragma once
+#include "hal/board.h"
+#include "hal/drivers/battery_analog.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
+#include "hal/drivers/keys_gpio.h"
+#include "hal/drivers/display_st7701s.h"
+#include "hal/drivers/backlight_ledc.h"
+#include "hal/drivers/battery_max17048.h"
+#include "hal/drivers/touch_ft5x06.h"
+#include "hal/drivers/keys_esp_io_expander.h"
+#include <driver/sdmmc_host.h>
+
+class board_2_1_v0_4 : public Board {
+ public:
+  board_2_1_v0_4();
+  ~board_2_1_v0_4() override = default;
+
+  std::shared_ptr<Battery> getBattery() override;
+  std::shared_ptr<Touch> getTouch() override;
+  std::shared_ptr<I2C> getI2c() override;
+  std::shared_ptr<Keys> getKeys() override;
+  std::shared_ptr<Display> getDisplay() override;
+  std::shared_ptr<Backlight> getBacklight() override;
+
+  void powerOff() override;
+  void pmLock() override {};
+  void pmRelease() override {};
+  BOARD_POWER powerState() override;
+  bool storageReady() override;
+  StorageInfo storageInfo() override;
+  esp_err_t StorageFormat() override { return ESP_OK; };
+  std::string name() override;
+  bool powerConnected() override;
+
+
+ private:
+  std::shared_ptr<battery_max17048> _battery;
+  std::shared_ptr<I2C> _i2c;
+  std::shared_ptr<keys_esp_io_expander> _keys;
+  std::shared_ptr<display_st7701s> _display;
+  std::shared_ptr<backlight_ledc> _backlight;
+  std::shared_ptr<touch_ft5x06> _touch;
+  sdmmc_card_t *card = nullptr;
+  esp_io_expander_handle_t _io_expander = nullptr;
+
+  typedef struct {
+    std::shared_ptr<battery_max17048> battery;
+    esp_io_expander_handle_t io_expander;
+  } batteryTimerArgs;
+
+  batteryTimerArgs _batteryTimerArgs;
+  static bool checkBatteryInstalled(esp_io_expander_handle_t io_expander);
+  static void batteryTimer(void *args);
+};
--- main/include/hal/boards/boards.h
+++ main/include/hal/boards/boards.h
@@ -3,3 +3,4 @@
 #define BOARD_1_28_V0 0
 #define BOARD_1_28_V0_1 2
 #define BOARD_2_1_V0_2 1
+#define BOARD_2_1_V0_4 3
