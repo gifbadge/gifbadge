3f1ad7286d8800772048ff348f40ed9c2993fbe9
gifbadge <gifbadge@gifbadge.ca>
2024-04-30 19:49:19 -0400

    Removed unused macro

--- main/include/ui/menu.h
+++ main/include/ui/menu.h
@@ -14,7 +14,6 @@
 #define LVGL_TICK_PERIOD_MS    5
 #define LVGL_TASK_MAX_DELAY_MS 500
 #define LVGL_TASK_MIN_DELAY_MS 1
-#define LVGL_TASK_STACK_SIZE   (4 * 1024)
 #define LVGL_TASK_PRIORITY     2
 
 struct flushCbData {
