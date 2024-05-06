ef488fff07de31efca2acb092da106e68e055b2c
gifbadge <gifbadge@gifbadge.ca>
2024-05-05 22:18:26 -0400

    Reduce use of std::string and std::filesystem

--- main/include/file_util.h
+++ main/include/file_util.h
@@ -6,3 +6,6 @@
 
 std::vector<std::string> list_folders(const std::string &path);
 std::vector<std::string> list_directory(const std::string &path);
+bool valid_file(const char *path);
+char *dirname (char *path);
+bool is_dir(const char *path);
