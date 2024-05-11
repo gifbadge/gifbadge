b89d75f52db31f03ae04461e7f927a4e3e8664a6
gifbadge <gifbadge@gifbadge.ca>
2024-05-11 00:50:07 -0400

    Remove unused functions

--- main/file_util.cpp
+++ main/file_util.cpp
@@ -49,12 +49,6 @@ char *dirname(char *path) {
   return path;
 }
 
-bool is_dir(const char *path){
-  struct stat buffer{};
-  stat(path, &buffer);
-  return (buffer.st_mode & S_IFDIR);
-}
-
 /*!
  * Checks if the file is not a directory, and not hidden
  * Only works on files in /data
@@ -81,68 +75,4 @@ bool valid_file(const char *path){
     }
   }
   return false;
-}
-
-std::vector<std::string> list_directory(const std::string &path) {
-  std::vector<std::string> files;
-  DIR *dir = opendir(path.c_str());
-  if (dir != nullptr) {
-    while (true) {
-      struct dirent *de = readdir(dir);
-      if (!de) {
-        break;
-      }
-      std::string current_path = (path + "/" + de->d_name);
-      if (current_path.starts_with("/data")) {
-        ESP_LOGV(TAG, "%s", current_path.c_str());
-        std::string fat_path = current_path.substr(5, current_path.length());
-        ESP_LOGV(TAG, "%s %s", current_path.c_str(), fat_path.c_str());
-        //Check if file is hidden, a directory, or has any ignored characters.
-        //This will only work on the first FatFS filesystem right now, I think?
-        FILINFO info;
-        if (f_stat(fat_path.c_str(), &info) == 0) {
-          ESP_LOGV(TAG, "%s %i %i", current_path.c_str(), info.fattrib & AM_DIR, info.fattrib & AM_HID);
-          if (!((info.fattrib & AM_DIR) || (info.fattrib & AM_HID))) {
-            if (de->d_name[0] != '.' && de->d_name[0] != '~') {
-              files.emplace_back(current_path);
-            }
-          }
-        }
-      }
-    }
-    closedir(dir);
-  }
-  return files;
-}
-
-std::vector<std::string> list_folders(const std::string &path) {
-  std::vector<std::string> folders;
-  DIR *dir = opendir(path.c_str());
-  if (dir != nullptr) {
-    while (true) {
-      struct dirent *de = readdir(dir);
-      if (!de) {
-        break;
-      }
-      std::string current_path = (path + "/" + de->d_name);
-      if (current_path.starts_with("/data")) {
-        ESP_LOGV(TAG, "%s", current_path.c_str());
-        std::string fat_path = current_path.substr(5, current_path.length());
-        ESP_LOGV(TAG, "%s %s", current_path.c_str(), fat_path.c_str());
-        //Check if file is hidden or has any ignored characters.
-        //This will only work on the first FatFS filesystem right now, I think?
-        FILINFO info;
-        if (f_stat(fat_path.c_str(), &info) == 0) {
-          ESP_LOGV(TAG, "%s %i %i", current_path.c_str(), info.fattrib & AM_DIR, info.fattrib & AM_HID);
-          if ((info.fattrib & AM_DIR) && !(info.fattrib & AM_HID)) {
-            if (de->d_name[0] != '.' && de->d_name[0] != '~') {
-              folders.emplace_back(current_path);
-            }
-          }
-        }
-      }
-    }
-    closedir(dir);
-  }
-  return folders;
 }
--- main/include/file_util.h
+++ main/include/file_util.h
@@ -4,8 +4,5 @@
 #include <dirent.h>
 #include <ff.h>
 
-std::vector<std::string> list_folders(const std::string &path);
-std::vector<std::string> list_directory(const std::string &path);
 bool valid_file(const char *path);
 char *dirname (char *path);
-bool is_dir(const char *path);
