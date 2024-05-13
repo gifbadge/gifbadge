b897215dff6ee6d753d676c94f65f90224107198
gifbadge <gifbadge@gifbadge.ca>
2024-05-12 21:28:18 -0400

    cleanup font symbols

--- main/include/ui/style.h
+++ main/include/ui/style.h
@@ -16,21 +16,6 @@ LV_FONT_DECLARE(battery_symbols_14)
 #define ICON_FOLDER_OPEN "\ue2c8"
 #define ICON_BACK "\ue166"
 #define ICON_UP "\ue5d8"
-#define ICON_CHARGING "\uea0b"
-#define ICON_CONNECTED "\uefe6"
-
-#define ICON_BATTERY_ALERT "\ue19c"
-#define ICON_BATTERY_REMOVED "\uf7ea"
-#define ICON_BATTERY_CHARGING_0 "\ue1a3"
-#define ICON_BATTERY_CHARGING_30 "\uf0a3"
-#define ICON_BATTERY_CHARGING_50 "\uf0a4"
-#define ICON_BATTERY_CHARGING_80 "\uf0a6"
-
-#define ICON_BATTERY_0 "\uebdc"
-#define ICON_BATTERY_30 "\uebe0"
-#define ICON_BATTERY_50 "\uebe2"
-#define ICON_BATTERY_80 "\uebd2"
-#define ICON_BATTERY_100 "\ue1a4"
 
 #define ICON_USB "\ue1e0"
 
