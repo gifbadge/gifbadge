469387d8d469dac8d12cfaed712ba660b538a428
gifbadge <gifbadge@gifbadge.ca>
2024-05-24 20:48:45 -0400

    Make font_render work with C code

--- non-idf-components/font_render/include/font_render.h
+++ non-idf-components/font_render/include/font_render.h
@@ -1,5 +1,10 @@
 #pragma once
 
-#include <cstdint>
-
+#include <stdint.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 int render_text_centered(int16_t x_max, int16_t y_max, int16_t margin, const char *text, uint8_t *buffer);
+#ifdef __cplusplus
+}
+#endif
