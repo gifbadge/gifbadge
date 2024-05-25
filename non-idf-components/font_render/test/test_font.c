bd46a1f0263de8d005f63d95f67a0767ea91e49a
gifbadge <gifbadge@gifbadge.ca>
2024-05-24 20:49:13 -0400

    Add a very basic test

---
+++ non-idf-components/font_render/test/test_font.c
@@ -0,0 +1,24 @@
+#include <malloc.h>
+#include "unity/unity.h"
+#include "font_render.h"
+
+void test_render_font(void) {
+  void *buffer = malloc(480 * 480 * 2);;
+  render_text_centered(480, 480, 10, "No Image", buffer);
+  free(buffer);
+}
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+  // clean stuff up here
+}
+
+int main(void) {
+  UNITY_BEGIN();
+
+  RUN_TEST(test_render_font);
+
+  return UNITY_END();
+}
