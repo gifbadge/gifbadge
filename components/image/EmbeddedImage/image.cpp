052db9703a6b2f31a790aa1f7dbfbc6094b878f6
gifbadge <gifbadge@gifbadge.ca>
2024-05-25 21:16:15 -0400

    use std::span for external access

--- components/image/EmbeddedImage/image.cpp
+++ components/image/EmbeddedImage/image.cpp
@@ -4,15 +4,15 @@
 #include "png.h"
 #include "gif.h"
 
-
-std::array<const char *, 4> extensions = {".gif", ".jpg", ".jpeg", ".png"};
+std::array<const char *, 4> extensionArray = {".gif", ".jpg", ".jpeg", ".png"};
+std::span<const char *> extensions(extensionArray);
 std::array<Image*(*)(), 4> handlers = {GIF::create, JPEG::create, JPEG::create, PNGImage::create};
 
 Image *ImageFactory(const char *path) {
   const char *ext = strrchr(path, '.');
   if(ext != nullptr) {
-    for (int i = 0; i < extensions.size(); i++) {
-      if (strcasecmp(extensions[i], ext) == 0) {
+    for (int i = 0; i < extensionArray.size(); i++) {
+      if (strcasecmp(extensionArray[i], ext) == 0) {
         return handlers[i]();
       }
     }
