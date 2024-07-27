63baf0989edcefb104301fe758c9d58034b363f3
gifbadge <gifbadge@gifbadge.ca>
2024-07-27 14:40:22 -0400

    Automate building

---
+++ main/ui/fonts/battery_symbols_14.c
@@ -0,0 +1,121 @@
+/*******************************************************************************
+ * Size: 14 px
+ * Bpp: 1
+ * Opts: --bpp 1 --size 14 --format lvgl --lv-include lvgl.h --font /home/gifbadge/esp32badge_software/MaterialSymbolsOutlined[FILL,GRAD,opsz,wght]-Filled.ttf -r0xea0b,0xf22f,0xe5cd --no-compress -o /home/gifbadge/esp32badge_software/main/ui/fonts/battery_symbols_14.c
+ ******************************************************************************/
+
+#ifdef LV_LVGL_H_INCLUDE_SIMPLE
+#include "lvgl.h"
+#else
+#include "lvgl.h"
+#endif
+
+#ifndef BATTERY_SYMBOLS_14
+#define BATTERY_SYMBOLS_14 1
+#endif
+
+#if BATTERY_SYMBOLS_14
+
+/*-----------------
+ *    BITMAPS
+ *----------------*/
+
+/*Store the image of the glyphs*/
+static LV_ATTRIBUTE_LARGE_CONST const uint8_t glyph_bitmap[] = {
+    /* U+E5CD "" */
+    0x81, 0x42, 0x24, 0x18, 0x18, 0x24, 0x42, 0x81,
+
+    /* U+EA0B "" */
+    0x6, 0x1, 0x80, 0xc0, 0x30, 0x1c, 0xf, 0xe3,
+    0xf9, 0xf4, 0x76, 0x7, 0x1, 0xc0, 0x60, 0x10,
+    0x0,
+
+    /* U+F22F "" */
+    0xf9
+};
+
+
+/*---------------------
+ *  GLYPH DESCRIPTION
+ *--------------------*/
+
+static const lv_font_fmt_txt_glyph_dsc_t glyph_dsc[] = {
+    {.bitmap_index = 0, .adv_w = 0, .box_w = 0, .box_h = 0, .ofs_x = 0, .ofs_y = 0} /* id = 0 reserved */,
+    {.bitmap_index = 0, .adv_w = 224, .box_w = 8, .box_h = 8, .ofs_x = 3, .ofs_y = 3},
+    {.bitmap_index = 8, .adv_w = 224, .box_w = 10, .box_h = 13, .ofs_x = 2, .ofs_y = 1},
+    {.bitmap_index = 25, .adv_w = 224, .box_w = 1, .box_h = 8, .ofs_x = 7, .ofs_y = 3}
+};
+
+/*---------------------
+ *  CHARACTER MAPPING
+ *--------------------*/
+
+static const uint16_t unicode_list_0[] = {
+    0x0, 0x43e, 0xc62
+};
+
+/*Collect the unicode lists and glyph_id offsets*/
+static const lv_font_fmt_txt_cmap_t cmaps[] =
+{
+    {
+        .range_start = 58829, .range_length = 3171, .glyph_id_start = 1,
+        .unicode_list = unicode_list_0, .glyph_id_ofs_list = NULL, .list_length = 3, .type = LV_FONT_FMT_TXT_CMAP_SPARSE_TINY
+    }
+};
+
+
+
+/*--------------------
+ *  ALL CUSTOM DATA
+ *--------------------*/
+
+#if LV_VERSION_CHECK(8, 0, 0)
+/*Store all the custom data of the font*/
+static  lv_font_fmt_txt_glyph_cache_t cache;
+static const lv_font_fmt_txt_dsc_t font_dsc = {
+#else
+static lv_font_fmt_txt_dsc_t font_dsc = {
+#endif
+    .glyph_bitmap = glyph_bitmap,
+    .glyph_dsc = glyph_dsc,
+    .cmaps = cmaps,
+    .kern_dsc = NULL,
+    .kern_scale = 0,
+    .cmap_num = 1,
+    .bpp = 1,
+    .kern_classes = 0,
+    .bitmap_format = 0,
+#if LV_VERSION_CHECK(8, 0, 0)
+    .cache = &cache
+#endif
+};
+
+
+/*-----------------
+ *  PUBLIC FONT
+ *----------------*/
+
+/*Initialize a public general font descriptor*/
+#if LV_VERSION_CHECK(8, 0, 0)
+const lv_font_t battery_symbols_14 = {
+#else
+lv_font_t battery_symbols_14 = {
+#endif
+    .get_glyph_dsc = lv_font_get_glyph_dsc_fmt_txt,    /*Function pointer to get glyph's data*/
+    .get_glyph_bitmap = lv_font_get_bitmap_fmt_txt,    /*Function pointer to get glyph's bitmap*/
+    .line_height = 13,          /*The maximum line height required by the font*/
+    .base_line = -1,             /*Baseline measured from the bottom of the line*/
+#if !(LVGL_VERSION_MAJOR == 6 && LVGL_VERSION_MINOR == 0)
+    .subpx = LV_FONT_SUBPX_NONE,
+#endif
+#if LV_VERSION_CHECK(7, 4, 0) || LVGL_VERSION_MAJOR >= 8
+    .underline_position = 0,
+    .underline_thickness = 0,
+#endif
+    .dsc = &font_dsc           /*The custom font data. Will be accessed by `get_glyph_bitmap/dsc` */
+};
+
+
+
+#endif /*#if BATTERY_SYMBOLS_14*/
+
