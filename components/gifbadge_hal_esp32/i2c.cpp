6219b67a1952dd1af412218abf9e94ad45a29b35
gifbadge <gifbadge@gifbadge.ca>
2024-09-08 12:14:04 -0400

    add 16 bit register support for i2c

--- components/gifbadge_hal_esp32/i2c.cpp
+++ components/gifbadge_hal_esp32/i2c.cpp
@@ -35,3 +35,23 @@ esp_err_t I2C::write_reg(uint8_t addr, uint8_t reg, uint8_t *in, size_t bytes) {
   free(to_write);
   return ret;
 }
+
+esp_err_t I2C::read_reg16(uint8_t addr, uint16_t reg, uint8_t *out, size_t bytes) {
+  const std::lock_guard<std::mutex> lock(i2c_lock);
+  uint8_t reg8[2];
+  reg8[0] = (reg >> 8)&0xFF;
+  reg8[1] = (reg &0xFF);
+  return i2c_master_write_read_device(_port, addr, reg8, 2, out, bytes, 100 / portTICK_PERIOD_MS);
+}
+
+esp_err_t I2C::write_reg16(uint8_t addr, uint16_t reg, uint8_t *in, size_t bytes) {
+  const std::lock_guard<std::mutex> lock(i2c_lock);
+  auto *to_write = static_cast<uint8_t *>(malloc(bytes + 2));
+  assert(to_write != nullptr);
+  to_write[0] = (reg >> 8);
+  to_write[1] = (reg &0x00FF);
+  memcpy(&to_write[2], in, bytes);
+  esp_err_t ret = i2c_master_write_to_device(_port, addr, to_write, bytes + 2, 100 / portTICK_PERIOD_MS);
+  free(to_write);
+  return ret;
+}
--- components/gifbadge_hal_esp32/include/i2c.h
+++ components/gifbadge_hal_esp32/include/i2c.h
@@ -8,6 +8,8 @@ class I2C {
   I2C(i2c_port_t port, int sda, int scl, uint32_t clk, bool pullup);
   esp_err_t read_reg(uint8_t addr, uint8_t reg, uint8_t *out, size_t bytes);
   esp_err_t write_reg(uint8_t addr, uint8_t reg, uint8_t *in, size_t bytes);
+  esp_err_t read_reg16(uint8_t addr, uint16_t reg, uint8_t *out, size_t bytes);
+  esp_err_t write_reg16(uint8_t addr, uint16_t reg, uint8_t *in, size_t bytes);
   std::mutex i2c_lock;
 //    ~I2C();
  private:
