cd906d1aa748c7cb3ec79cdee0ef171321d253b0
gifbadge <gifbadge@gifbadge.ca>
2024-11-08 22:09:10 -0500

    Add documentation

--- non-idf-components/gifbadge_hal/include/hal/battery.h
+++ non-idf-components/gifbadge_hal/include/hal/battery.h
@@ -1,5 +1,9 @@
 #pragma once
 #include <cmath>
+
+/**
+ * Abstraction for battery hardware
+ */
 class Battery {
  public:
 
@@ -19,17 +23,43 @@ class Battery {
 
   virtual int pollInterval() = 0;
 
+  /**
+   * Get the battery voltage
+   * @return voltage in V
+   */
   virtual double BatteryVoltage() = 0;
 
+  /**
+   * Get the current being sourced or sunk by the battery
+   * @return current in A
+   */
   virtual double BatteryCurrent() { return NAN; }
 
+  /**
+   * Get the battery temperature
+   * @return Temperature in °C
+   */
   virtual double BatteryTemperature() { return NAN;}
 
+  /**
+   * Get the state of charge of the battery
+   * @return charge in percent
+   */
   virtual int getSoc() = 0;
 
+  /**
+   * Notify hardware that battery has been removed
+   */
   virtual void removed() = 0;
 
+  /**
+   * Notify hardware that battery has been inserted
+   */
   virtual void inserted() = 0;
 
+  /**
+   * Get the current status of the battery hardware
+   * @return
+   */
   virtual State status() = 0;
 };
