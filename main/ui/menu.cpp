7a3def81ed15a692f25ae966f9b29521ba7026d7
gifbadge <gifbadge@gifbadge.ca>
2025-12-08 12:20:32 -0500

    Use spi ram for LVGL temporarily

--- main/ui/menu.cpp
+++ main/ui/menu.cpp
@@ -200,9 +200,16 @@ void lvgl_init(Boards::Board *board) {
   flushSem = xSemaphoreCreateBinary();
   lvgl_open = xSemaphoreCreateRecursiveMutex();
 
+#ifdef ESP_PLATFORM
   // tickHandle = xTimerCreate("lvglTick", 5/portTICK_PERIOD_MS, pdTRUE, nullptr, lv_tick);
+  auto *xTaskBuffer =  static_cast<StaticTask_t *>(heap_caps_malloc(sizeof(StaticTask_t), MALLOC_CAP_SPIRAM));
+  auto *xStack = static_cast<StackType_t *>(heap_caps_malloc(7*1024, MALLOC_CAP_SPIRAM));
 
+  lvgl_task = xTaskCreateStatic(task, "LVGL", 7*1024, nullptr, LVGL_TASK_PRIORITY, xStack, xTaskBuffer);
+  // xTaskCreate(task, "LVGL", 5*1024, nullptr, LVGL_TASK_PRIORITY, &lvgl_task);
+#else
   xTaskCreate(task, "LVGL", 7*1024, nullptr, LVGL_TASK_PRIORITY, &lvgl_task);
+#endif
 
   disp = lv_display_create(board->GetDisplay()->size.first, board->GetDisplay()->size.second);
   lv_display_set_flush_cb(disp, flush_cb);
