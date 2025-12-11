5076d389ff1a73dc4b303516d5973012584984d9
gifbadge <gifbadge@gifbadge.ca>
2025-12-11 18:32:28 -0500

    Fix formatting

--- idf-components/gifbadge_hal_esp32/include/boards/boards_esp32s3.h
+++ idf-components/gifbadge_hal_esp32/include/boards/boards_esp32s3.h
@@ -5,42 +5,40 @@
 #include "drivers/config_nvs.h"
 #include "tinyusb_msc.h"
 
-
 namespace Boards::esp32::s3 {
 class esp32s3 : public Board {
- public:
-  size_t MemorySize() override;
+  public:
+    size_t MemorySize() override;
 
-  esp32s3();
- ~esp32s3() override = default;
+    esp32s3();
+    ~esp32s3() override = default;
 
- void PmLock() override;
- void PmRelease() override;
+    void PmLock() override;
+    void PmRelease() override;
 
- hal::config::Config *GetConfig() override;
- void DebugInfo() override;
- const char *SwVersion() override;
-  void Reset() override;
-  char *SerialNumber() override;
-  void BootInfo() override;
-  bool OtaCheck() override;
- static OtaError OtaHeaderValidate(uint8_t const *data);
- OtaError OtaValidate() override;
-  void OtaInstall() override;
-  int OtaStatus() override;
+    hal::config::Config *GetConfig() override;
+    void DebugInfo() override;
+    const char *SwVersion() override;
+    void Reset() override;
+    char *SerialNumber() override;
+    void BootInfo() override;
+    bool OtaCheck() override;
+    static OtaError OtaHeaderValidate(uint8_t const *data);
+    OtaError OtaValidate() override;
+    void OtaInstall() override;
+    int OtaStatus() override;
 
- protected:
- esp_pm_lock_handle_t pmLockHandle = nullptr;
- hal::config::esp32s3::Config_NVS *_config;
- char serial[18] = {0};
-  tinyusb_msc_storage_handle_t storage_handle = nullptr;
-  char storage_base_path[6] = "/data";
+  protected:
+    esp_pm_lock_handle_t pmLockHandle = nullptr;
+    hal::config::esp32s3::Config_NVS *_config;
+    char serial[18] = {0};
+    tinyusb_msc_storage_handle_t storage_handle = nullptr;
+    char storage_base_path[6] = "/data";
 
- static void OtaInstallTask(void *arg);
-  TaskHandle_t _ota_task_handle = nullptr;
-  int _ota_status = -1;
-
+    static void OtaInstallTask(void *arg);
+    TaskHandle_t _ota_task_handle = nullptr;
+    int _ota_status = -1;
 };
 }
 
-char* lltoa(long long val, int base);
+char *lltoa(long long val, int base);
