1424fa4fdc2d937f359111f79f8c51def51ddfe8
gifbadge <gifbadge@gifbadge.ca>
2025-12-11 18:30:22 -0500

    Cleanup unused imports

--- idf-components/gifbadge_hal_esp32/include/boards/boards_esp32s3.h
+++ idf-components/gifbadge_hal_esp32/include/boards/boards_esp32s3.h
@@ -1,19 +1,7 @@
 #pragma once
 
-#pragma once
 #include <hal/board.h>
 #include <esp_pm.h>
-#include "drivers/battery_analog.h"
-#include "esp_lcd_panel_io.h"
-#include "esp_lcd_panel_vendor.h"
-#include "esp_lcd_panel_ops.h"
-#include "drivers/keys_gpio.h"
-#include "drivers/display_st7701s.h"
-#include "drivers/backlight_ledc.h"
-#include "drivers/battery_max17048.h"
-#include "drivers/touch_ft5x06.h"
-#include "drivers/keys_esp_io_expander.h"
-#include "driver/sdmmc_host.h"
 #include "drivers/config_nvs.h"
 #include "tinyusb_msc.h"
 
