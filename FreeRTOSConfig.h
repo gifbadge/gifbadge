0626a5e5014c6c73143844e32ba261e32360ed8c
gifbadge <gifbadge@gifbadge.ca>
2025-12-27 17:48:37 -0500

    Re-enable tick hook

--- FreeRTOSConfig.h
+++ FreeRTOSConfig.h
@@ -40,7 +40,7 @@
 #define configUSE_PREEMPTION                       1
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION    0
 #define configUSE_IDLE_HOOK                        0
-#define configUSE_TICK_HOOK                        0
+#define configUSE_TICK_HOOK                        1
 #define configUSE_DAEMON_TASK_STARTUP_HOOK         0
 #define configTICK_RATE_HZ                         ( 1000 )                  /* In this non-real time simulated environment the tick frequency has to be at least a multiple of the Win32 tick frequency, and therefore very slow. */
 #define configMINIMAL_STACK_SIZE                   ( ( unsigned short ) PTHREAD_STACK_MIN ) /* The stack size being passed is equal to the minimum stack size needed by pthread_create(). */
