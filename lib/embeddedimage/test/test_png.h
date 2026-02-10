9712af3fa346519f86f912834721a28eb1f5096d
gifbadge <gifbadge@gifbadge.ca>
2026-02-10 14:21:05 -0500

    jpeg and png basic tests

---
+++ lib/embeddedimage/test/test_png.h
@@ -0,0 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2026 GifBadge
+ *
+ * SPDX-License-Identifier:   GPL-3.0-or-later
+ ******************************************************************************/
+
+#pragma once
+
+void test_png_480_open();
+void test_png_480_redraw();
+void test_png_480i_open();
+void test_png_240_on_480();
+void test_png_437_on_480();
